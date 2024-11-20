472372ca395a8bb455dabd2a088d9f0c64bd27f5
Richard Hitier <hitier.richard@gmail.com>
2024-11-20 15:16:27 +0100

    Insert graph img into web page

--- web/main/routes.py
+++ web/main/routes.py
@@ -1,8 +1,11 @@
+import base64
+from io import BytesIO
 
 from flask import redirect, url_for, render_template
 
 from . import bp
 from ..tools.histories import repo_to_df, hours_per_day, merge_histories
+from ..tools.plots import plot_df
 
 
 @bp.route("/")
@@ -16,4 +19,9 @@ def commits(project_name):
     later_date = datetime.now()
     sooner_date = later_date - timedelta(days=120)
     hits_df = merge_histories(project_name).truncate(before=sooner_date, after=later_date)
-    return render_template("commits.html", hits=hits_df.to_html())
+    hits_fig = plot_df(hits_df)
+    buf = BytesIO()
+    hits_fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    img_data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return render_template("commits.html", hits=hits_df.to_html(), img_data=img_data)
