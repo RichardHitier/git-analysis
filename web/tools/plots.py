e6d0348448ea2433e1d197e534c90e49e1f0cfd6
Richard Hitier <hitier.richard@gmail.com>
2024-11-20 15:16:07 +0100

    New plot_df() method returns base64

--- web/tools/plots.py
+++ web/tools/plots.py
@@ -6,38 +6,47 @@ from datetime import date, timedelta
 
 from web.tools.histories import merge_histories
 
-date_format = '%d %b'
 
-df = merge_histories("calipso")
+def plot_df(df):
+    date_format = '%d %b'
 
-# get current month day
-today = date.today()
+    # get current month day
+    today = date.today()
 
-month_before = today - timedelta(days=60)
-month_after = today + timedelta(days=30)
+    month_before = today - timedelta(days=60)
+    month_after = today + timedelta(days=30)
 
-pd_dr = pd.date_range(start=month_before, end=month_after, freq="D")
+    pd_dr = pd.date_range(start=month_before, end=month_after, freq="D")
 
-df = df.reindex(pd_dr)
+    df = df.reindex(pd_dr)
+
+    fig, ax = plt.subplots(3, figsize=(20, 8), sharex=True)
+
+    ax[0].tick_params(axis='x', labelsize=10, rotation=30)
+
+    ax[0].xaxis.set_major_formatter(mdates.DateFormatter(date_format))
+    ax[0].set_title("Number of Commits per day")
+    bc = ax[0].bar(pd_dr, df.nb_commits, color="red", width=0.8, edgecolor="black")
+
+    ax[1].tick_params(axis='x', labelsize=10, rotation=30)
+    ax[1].xaxis.set_major_formatter(mdates.DateFormatter(date_format))
+    ax[1].set_title("Worked hours per day")
+    cbc = ax[1].bar(pd_dr, df.duration_hour, color="blue", width=0.8, edgecolor="black")
+
+    ax[2].tick_params(axis='x', labelsize=10, rotation=30)
+    ax[2].xaxis.set_major_formatter(mdates.DateFormatter(date_format))
+    ax[2].set_title("Pomodoros per day")
+    ax[2].set_xticks(pd_dr)
+    pbc = ax[2].bar(pd_dr, df.minutes, width=0.5)
+
+    return fig
 
 
-fig, ax = plt.subplots(3, figsize=(20, 8), sharex=True)
+if __name__ == "__main__":
+    df = merge_histories("calipso")
 
-ax[0].tick_params(axis='x', labelsize=10, rotation=30)
+    my_fig = plot_df(df)
 
-ax[0].xaxis.set_major_formatter(mdates.DateFormatter(date_format))
-ax[0].set_title("Number of Commits per day")
-bc = ax[0].bar(pd_dr, df.nb_commits, color="red", width=0.8, edgecolor="black")
+    # my_fig.show()
 
-ax[1].tick_params(axis='x', labelsize=10, rotation=30)
-ax[1].xaxis.set_major_formatter(mdates.DateFormatter(date_format))
-ax[1].set_title("Worked hours per day")
-cbc = ax[1].bar(pd_dr, df.duration_hour, color="blue", width=0.8, edgecolor="black")
-
-ax[2].tick_params(axis='x', labelsize=10, rotation=30)
-ax[2].xaxis.set_major_formatter(mdates.DateFormatter(date_format))
-ax[2].set_title("Pomodoros per day")
-ax[2].set_xticks(pd_dr)
-pbc = ax[2].bar(pd_dr, df.minutes, width=0.5)
-
-plt.show()
+    plt.show()
